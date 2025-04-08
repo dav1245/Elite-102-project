d893fc4e37390828e06f4ad91fbfd06946e95827
dav1245 <dfboi123@gmail.com>
2025-04-08 00:10:38 +0000

    yes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print ("Welcome to the amazing.. and mediocre.. app! Here's the stuff: ")
+print ("Welcome to the amazing.. and mediocre.. clothing app! Here's the stuff: ")
 Menu = ['First: Shirts', 'Second: Shoes', 'Third: TVs', 'X-it: Exit']
 for i in sorted(Menu):
     print(i)
