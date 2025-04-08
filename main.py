c55c13382765e583c2de1eb27bcbf45c652adc37
dav1245 <dfboi123@gmail.com>
2025-04-07 20:06:22 -0400

    Update main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+print ("Welcome to the amazing.. and mediocre.. app! Here's the stuff: ")
+Menu = ['First: Shirts', 'Second: Shoes', 'Third: TVs', 'X-it: Exit']
+for i in sorted(Menu):
+    print(i)
